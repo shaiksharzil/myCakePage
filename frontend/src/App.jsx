655851bc31360697064647ba0dac7342227e99a4
shaiksharzil <shaiksharzil@gmail.com>
2025-06-25 18:52:20 +0530

    scrollbar color change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,6 +43,10 @@ const App = () => {
                   background: "#000",
                   color: "#fff",
                 },
+                iconTheme: {
+                  primary: "white",
+                  secondary: "black",
+                },
               }}
             />
             <div className="flex-1">
